9e7e8da96684a35c4fc5a8c9c93bcfb60e5ab54f
barrosads <alvesdebarrosf@gmail.com>
2025-04-05 16:30:33 -0300

    Update movimentacaoestoque.js

--- src/pages/movimentacaoestoque.js
+++ src/pages/movimentacaoestoque.js
@@ -1,7 +1,7 @@
 function Movimentacaoestoque() {
     return (
       <div className="App">
-       <h1>Professor estou terminando </h1>
+       <h1> Professor estou terminando hoje 05/04/2025</h1>
       </div>
     );
   }
