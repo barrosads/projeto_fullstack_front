b4a58e2d0184a87fd0931e2042df69cb3761c4de
Fabio Alves <alvesdebarrosf@gmail.com>
2025-04-06 20:22:08 -0300

    acrescentado botao voltar

--- src/pages/relatorioestoque.js
+++ src/pages/relatorioestoque.js
@@ -51,7 +51,7 @@ const EstoqueMovimentacoes = () => {
 
   return (
     <div className="relatorioestoque">
-    <Link className="linkform" to="/"> Página inicial</Link>
+    <Link className="linkform2" to="/"> Página inicial</Link>
       <h2>Movimentações de Estoque</h2>
       {error ? (
         <p style={{ color: "red" }}>{error}</p>
